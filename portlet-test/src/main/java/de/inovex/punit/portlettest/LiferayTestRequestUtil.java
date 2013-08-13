4e486f3595c4a7f36893ad52059c407dd9b99c20
Andreas Friedel <andreas.friedel@inovex.de>
2013-08-13 16:31:43 +0200

    fixed header

--- portlet-test/src/main/java/de/inovex/punit/portlettest/LiferayTestRequestUtil.java
+++ portlet-test/src/main/java/de/inovex/punit/portlettest/LiferayTestRequestUtil.java
@@ -22,6 +22,23 @@ import com.liferay.portal.model.LayoutMock;
 import com.liferay.portal.model.UserMock;
 import com.liferay.portal.theme.ThemeDisplay;
 
+/**
+ * 
+ * Copyright (C) 2013 Andreas Friedel - https://github.com/inovex/PUnit - <inovex GmbH>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ * 
+ */
 public class LiferayTestRequestUtil {
 	
 	private final static Logger LOG = LoggerFactory.getLogger(LiferayTestRequestUtil.class);
--- portlet-test/src/main/java/de/inovex/punit/portlettest/Scenario.java
+++ portlet-test/src/main/java/de/inovex/punit/portlettest/Scenario.java
@@ -6,6 +6,23 @@ import org.springframework.mock.web.portlet.MockActionResponse;
 import org.springframework.mock.web.portlet.MockRenderRequest;
 import org.springframework.mock.web.portlet.MockRenderResponse;
 
+/**
+ * 
+ * Copyright (C) 2013 Andreas Friedel - https://github.com/inovex/PUnit - <inovex GmbH>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ * 
+ */
 public interface Scenario extends TestRule {
 
 	public MockActionRequest getActionRequest();
